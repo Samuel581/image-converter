4219b4d891a43e176ba4890ce0f26f7ffb0d8b1b
Samuel Ortiz <00023020@uca.edu.sv>
2024-09-12 16:34:47 -0600

    :ambulance: Hotfix about syntax
    Fixed bad syntax on variable asignment and if statement, script didn't work well before that

--- image_converter.sh
+++ image_converter.sh
@@ -14,10 +14,10 @@ for file in *.webp; do
   fi
   
   # {$file} takes the whole file.format variable but if we add "%" we "cut" the format part of the variable
-  output = "${file%.webp}.png"
+  output="${file%.webp}.png"
   convert "$file" "$output"
 
-  if [$ -eq 0]; then
+  if [ $? -eq 0 ]; then
     echo "$file has been converted to $output"
   else
     echo "An error occurred while converting $file"
